7b9d9581a42787ebbd83ce8dfc5024678a1ce568
Balaji Srinivas <balaji21srinivas@gmail.com>
2024-03-06 12:27:09 +0530

    Final Commit

---
+++ Moonshot/AstronautView.swift
@@ -0,0 +1,18 @@
+//
+//  AstronautView.swift
+//  Moonshot
+//
+//  Created by Balaji Srinivas on 06/03/24.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AstronautView()
+}
